9a1d8cf60ca9c857a55b373600e94e6b6d890cc7
Oussama <195052@ecam.be>
2025-05-11 00:19:32 +0200

    DETAILS MATCHS- Add Button Back

--- src/routes/match/[id].tsx
+++ src/routes/match/[id].tsx
@@ -7,12 +7,18 @@ export default function MatchPage(props: { params: { id: any; }; }) {
   const match = createAsync(() => getMatchById(props.params.id));
 
   return (
-    <main class="ml-48 text-white p-8">
-      <h1 class="text-5xl font-bold uppercase mb-8 text-center">Détails du Match</h1>
+<main class="ml-48 text-white p-8 relative">
+  {/* Bouton retour positionné en haut à gauche */}
+  <a href="/" class="absolute top-5 left-14">
+    <img src="/images/buttonback.png" alt="Retour" class="w-16 h-16" />
+  </a>
 
-      <Show when={match()}>
-        {(data) => <MatchCard match={data()} />}
-      </Show>
-    </main>
+  {/* Titre centré */}
+  <h1 class="text-5xl font-bold uppercase text-center mb-8">Détails du Match</h1>
+
+  <Show when={match()}>
+    {(data) => <MatchCard match={data()} />}
+  </Show>
+</main>
   );
 }
